18e7aa1983af6faad24d502c9e4d1a131e165be4
chauhoang99er <63139849+chauhoang99er@users.noreply.github.com>
2020-04-13 09:58:29 +0700

    Initial commit

---
+++ app/src/main/java/com/chauhoang/nnc/nguyenngocchauhoang_59130798_androidlifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chauhoang.nnc.nguyenngocchauhoang_59130798_androidlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
